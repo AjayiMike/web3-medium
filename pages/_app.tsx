7597512e34116a7244589bd6a6ff519fb7e62a96
AjayiMike <ajayimikeofficial@gmail.com>
2022-06-02 02:23:05 +0100

    work on landing page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Fragment } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <Fragment>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin="anonymous"
+                />
+                <link href="https://fonts.googleapis.com/css2?family=Blaka&family=Kanit:ital,wght@1,700&display=swap" rel="stylesheet" />
+            </Head>
+            <Component {...pageProps} />
+        </Fragment>
+    );
 }
 
-export default MyApp
+export default MyApp;
